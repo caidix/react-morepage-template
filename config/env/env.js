a4d62189d02512d7322f973c9dceb50e889ce60b
caidi <380141132@qq.com>
2020-06-23 16:20:01 +0800

    ':art:增加配置'

--- config/env/env.js
+++ config/env/env.js
@@ -13,6 +13,12 @@ class EnvContainer {
   setModule(module = '') {
     this.module = path.join(module, env.wideModule); // "wideModule": "**/**"设置module。在运行时可以做配置如 传入setModule('src/pages/')
   }
+  register(name, value) {
+    this.extras.set(name, value);
+  }
+  getExtra(name) {
+    return this.extras.get(name);
+  }
 }
 
 exports = module.exports = new EnvContainer();
